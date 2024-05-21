fa5b647cdfbd6cece9d8f992ab8f3abdada4ff79
tew9 <tango.tew1@accesscfa.com>
2024-05-21 09:11:45 -0500

    quantize real llm models

--- linear_quantizer/helpers.py
+++ linear_quantizer/helpers.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -85,3 +87,89 @@ def quantize_linear_W8A32_without_bias(input, quantized_w, scale_w, zeropoint_w)
   #make inference using linear layer
   output = torch.nn.functional.linear(input, dequantized_weights)
   return output
+
+def w8_a16_forward(weight, input, scale, bias=None):
+  # Cast weight to input type
+  casted_weight = weight.to(input.dtype)
+  # Perform matrix multiplication
+  matrix_mul = F.linear(input, casted_weight) * scale
+  # Add bias
+  if bias is not None:
+    matrix_mul += bias
+  return matrix_mul
+
+class W8A16LinearLayer(nn.Module):
+    def __init__(self, in_features, out_features, 
+                bias=True, dtype=torch.float32):
+        super().__init__()
+        
+        self.register_buffer( 
+            "int8_weights",
+            torch.randint(-128, 127, (out_features, in_features)).to(torch.int8)
+        )
+
+        self.register_buffer( 
+            "scale",
+            torch.randn((out_features), dtype=dtype)
+        )
+        if bias:
+            self.register_buffer( 
+                "bias",
+                torch.randn((1, out_features), dtype=dtype)
+            )
+        else:
+            self.bias = None
+
+    # Add quantizer
+    def quantize(self, weights):
+        # cast weight to float32
+        w_fp32 = weights.to(torch.float32)
+        # get the scale by getting absolute max value of the the last dimension
+        scales = w_fp32.abs().max(dim=-1).values / 127
+        scales = scales.to(weights.dtype)
+
+        int8_weights = torch.round(weights / scales.unsqueeze(1)).to(torch.int8)
+        self.int8_weights = int8_weights
+
+        self.scale = scales
+
+    def forward(self, input):
+        return w8_a16_forward(self.int8_weights, input, self.scale, self.bias)
+    
+
+# loop over torch.nn.Module children and replace them with a new module
+def replace_linear_with_target_and_quantize(module, target_class, module_names_to_exclude):
+  '''
+  Replace all linear layers in a module with a target class.
+
+  Parameters:
+  - module: The module containing the linear layers to be replaced.
+  - target_class: The target class to replace the linear layers with.
+  - module_names_to_exclude: A list of module names to exclude from replacement.
+
+  Returns:
+  - The modified module with linear layers replaced by the target class.
+  '''
+  for name, child in module.named_children():
+    if isinstance(child, torch.nn.Linear) and not any([x ==name for x in module_names_to_exclude]):
+      # get old module bias
+      old_bias = child.bias
+      # retrieve the old weight
+      old_weight = child.weight
+
+      # create new module 
+      new_module = target_class(child.in_features, child.out_features, old_bias is not None, child.weight.dtype)
+
+      # replace current module name with new module
+      setattr(module, name, new_module)
+
+      # Once the old module is replaced above, we can now set the old weight to the new module
+      # Get this new module, and quantize it's old weight
+      getattr(module, name).quantize(old_weight)
+
+      if old_bias is not None:
+        # if old module had bias, set the bias of new module to the old bias
+        getattr(module, name).bias.data = old_bias
+    else:
+      # Recursively apply the function to the child module for nested modules
+      replace_linear_with_target_and_quantize(child, target_class, module_names_to_exclude)
